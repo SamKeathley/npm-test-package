464fd8c23a9de9d9231590e741c1ffdee477b603
Sam K <samncollette@gmail.com>
2018-01-08 12:09:08 -0600

    first commit

---
+++ tests/assertion-test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect
+var assert = require('assert');
+describe('Array', function() {
+  describe('asserting', function() {
+    it('works', function() {
+      expect(1).to.equal(1);
+  });
+});
+});
